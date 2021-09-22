328e02eee7d310085a74beef871fdfc98da9f4dd
Joshua Allford <joshallfordnanook@gmail.com>
2021-09-22 01:02:21 +0100

    Assignment Complete!

--- src/components/App.js
+++ src/components/App.js
@@ -5,11 +5,11 @@ function App() {
   const title = "Mad Max";
   const posterURL =
     "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTn1OTYGz2GDC1XjA9tirh_1Rd571yE5UFIYsmZp4nACMd7CCHM";
-  const genresArr = ["Action", "Adventure", "Science Fiction", "Thriller"];
+  const genresArr = ["Action", "Adventure", "Science Fiction", "Thriller","Bad ass Movie"];
 
   return (
     <div className="App">
-      <MovieCard title={title} genres={genresArr} />
+      <MovieCard title={title} genres={genresArr} posterSrc={posterURL} />
     </div>
   );
 }
